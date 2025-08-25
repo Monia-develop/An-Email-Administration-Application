af512da832918855ff298483f3ead2167f60a524
Monia-develop <moniaazzaiz2@gmail.com>
2025-08-25 20:47:48 +0200

    Add email generation, mailbox capacity, and alternate email features
    - Email address now built from first name, last name, department and company suffix
    - Added methods to set mailbox capacity, alternate email, and password
    - Added getters to retrieve mailbox capacity, alternate email, and password
    - Updated EmailApp to test setting an alternate email

--- Email.java
+++ Email.java
@@ -5,9 +5,11 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
-    private int mailboxCapacity;
+    private String email;
+    private int mailboxCapacity = 500;
     private int defaultPasswordLength = 10;
     private String AlternateEmail;
+    private String companySuffix = "aeycompany.com";
 
 
 
@@ -24,6 +26,10 @@ public class Email {
         //call a method that returns a random password
         this.password = randomPassword(defaultPasswordLength);
         System.out.println("Your password is: " + this.password);
+
+        //Combine elements to generate email
+         email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+         System.out.println("Your email is: " + email);
     }
 
     //ask for the departement
@@ -44,6 +50,7 @@ public class Email {
             return "";
         }
     }
+        //Generate a random password 
     private String randomPassword(int length) {
         String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
            char[] password = new char[length];
@@ -53,8 +60,27 @@ public class Email {
            } 
            return new String(password); 
         }
-}
-    //Generate a random password 
     //Set the mailbox capacity
+    public void setMailBoxCapacity(int capacity){
+        this.mailboxCapacity = capacity;
+    }
+
     //Set the alternate email
+    public void setAlternateEmail(String altEmail){
+        this.AlternateEmail = altEmail;
+    }
+
     //Change the password
+    public void changePassword(String password){
+        this.password = password;
+    }
+    public int getMailBoxCapacity() {
+        return mailboxCapacity;
+    }
+    public String getAlternateEmail(){
+        return AlternateEmail;
+    }
+    public String getPassword(){
+        return password;
+    }
+}
