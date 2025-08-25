127321df5d297f76a70ba7541fb99b18cb6b6e08
Monia-develop <moniaazzaiz2@gmail.com>
2025-08-25 18:39:28 +0200

    Add random password generation and department selection to Email class
    - Implemented randomPassword() method to generate secure passwords
    - Added defaultPasswordLength to define the passwrd length
    - Updated constructor to set department and password automatically

--- Email.java
+++ Email.java
@@ -4,8 +4,9 @@ public class Email {
     private String firstName;
     private String lastName;
     private String password;
-    private String departement;
+    private String department;
     private int mailboxCapacity;
+    private int defaultPasswordLength = 10;
     private String AlternateEmail;
 
 
@@ -16,13 +17,18 @@ public class Email {
         this.lastName = lastName;
         System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
 
-        //Call a method asking for the departement - return the departement
-        this.departement = setDepartement();
+        //Call a method asking for the department - return the departement
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+
+        //call a method that returns a random password
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your password is: " + this.password);
     }
 
     //ask for the departement
-    private String setDepartement() {
-        System.out.print("Enter the departement\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none");
+    private String setDepartment() {
+        System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter the department code: ");
         Scanner in = new Scanner(System.in);
         int depChoice = in.nextInt();
         if (depChoice == 1) {
@@ -38,8 +44,17 @@ public class Email {
             return "";
         }
     }
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+           char[] password = new char[length];
+           for (int i=0; i<length;i++){
+          int rand = (int) (Math.random() * passwordSet.length());
+          password[i] = passwordSet.charAt(rand);
+           } 
+           return new String(password); 
+        }
+}
     //Generate a random password 
     //Set the mailbox capacity
     //Set the alternate email
     //Change the password
-}
